b6b7114f25c934bc7b0518a50099229bf77578fd
zakk125 <vannzakk@gmail.com>
2026-02-28 09:52:15 +0700

    news

--- src/app/services/matchmaking.ts
+++ src/app/services/matchmaking.ts
@@ -37,7 +37,7 @@ import { io } from 'socket.io-client';
 
 @Injectable({ providedIn: 'root' })
 export class MatchmakingService {
-  private socket = io('https://wet-tumor-closer-dried.trycloudflare.com');
+  private socket = io('https://september-document-cheap-cloud.trycloudflare.com');
   private router = inject(Router);
   
   isSearching = signal(false);
